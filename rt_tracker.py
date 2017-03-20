fbe25e1c7465e81285016ed16235bb10e8d534da
Matthew Streatfield <matt.streatfield@sol1.com.au>
2017-03-21 10:11:46 +1100

    renamed files

---
+++ rt_tracker.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Icinga2 plugin to create and track RT tickets when services and hosts go critical"""
+
+def create_ticket():
+	print("Hello world!")
+	return
+
+createTicket()
