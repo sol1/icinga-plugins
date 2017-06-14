79025efaef82390c7dcedca425885084e344295d
Oliver Lowe <oliver.lowe@sol1.com.au>
2017-06-14 13:23:36 +1000

    Functional implementation of check_age.
    But not complete with docs and build/install instructions

--- check_age/check_age.go
+++ check_age/check_age.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"flag"
+	"math"
 	"os"
 	"time"
 )
@@ -13,23 +15,45 @@ func modtime(path string) (time time.Time, err error) {
 		fmt.Fprintf(os.Stderr, "Error opening %s\n", path);
 		return time, err;
 	} else {
-		fmt.Printf("Successful stat() on %s\n", path);
+		fmt.Fprintf(os.Stderr, "Successful stat() on %s\n", path);
 		return fi.ModTime(), nil;
 	}
 }
 
 func main() {
-	var path string;
-	var ret int;
-	path = os.Args[1];
+	var path string
+	var warn, crit *float64;
+	var now time.Time;
+	var offset time.Duration;
+	var diff float64;
 
-	ret = 0;
+	warn = flag.Float64("w", 1800, "warning threshold");
+	crit = flag.Float64("c", 3600, "critical threshold");
+	flag.Parse();
+	path = flag.Arg(0);
+
+	/* Start with an unknown status */
+	var ret int;
+	ret = 3;
+
 	mt, err := modtime(path);
 	if (err != nil) {
-		ret = 1
-	} else {
-		fmt.Print(mt);
+		fmt.Fprintf(os.Stderr, "Error checking modification time of %s:",
+		    path)
 	}
 
+	now = time.Now();
+	offset = now.Sub(mt);
+
+	diff = math.Abs(float64(offset.Seconds()))
+	if (diff >= *crit) {
+		ret = 2;
+	} else if (diff >= *warn) {
+		ret = 1;
+	} else if (diff < *warn) {
+		ret = 0;
+	}
+
+	fmt.Printf("%s modified %g seconds ago\n", path, diff);
 	os.Exit(ret);
-};
+}
