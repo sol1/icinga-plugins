35ebd01dd570c3fb193ad86ae1cfbfbca0d8ce23
root <root@subview.sol1.net>
2017-07-27 11:47:09 +1000

    1.4 check zimbra backups  - add file checks to files to make sure they are fresh  - add check template Matt

--- check_zimbra_backups.sh
+++ check_zimbra_backups.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Zimbra per-account backup check.
+# Vesion 1.4 update by Matt
+# - Add file checks
+# - Add check boilerplate
 # Vesion 1.3 rewritten by Oli
 # Setup cron job to list accounts existing at 3AM for each server.
 # Compares the backed up accounts against that list, rather than a new list each time which potentially contains new accounts
@@ -8,57 +11,112 @@
 #59 02 * * * zimbra zmprov -l gaa -s subsoil.colo.sol1.net > /tmp/originalmailboxes.txt
 
 
-if [ $# -eq 0 ]
-  then
-        echo "No arguments supplied - Specify the server to check for backups"
-        exit 255
+function checkParam() {
+	local name="$1"
+	local param="$2"
 
-fi
+	if [ -z "$param" ] ; then
+		MESSAGE="${MESSAGE} $name"
+	fi
+}
 
-CHECK=Unknown
-SERVER=$1
-BACKUP_TXT="/tmp/$SERVER-backups.txt"
-STATUS_TXT="/tmp/$SERVER-status.txt"
-MAILBOXES_TXT="/tmp/originalmailboxes.txt"
+function trimMessage() {
+	MESSAGE="$(echo -e "${MESSAGE}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' )"
+}
 
-
-checkexitstatus () {
-        if [ "$?" != "0" ]
-                then echo "Failed to check server $SERVER"
-                exit 3
+function checkexitstatus () {
+        if [ "$?" != "0" ] ; then 
+		echo "UNKNOWN - Failed to check server $SERVER"
+                exit $STATE_UNKNOWN
         fi
 }
 
+function checkfileage () {
+	local file="$1"
+	local warn="$2"
+	local crit="$3"
+	local message=""
+	
+	# If we have a remote target use it
+	if [ -n "$4" ] ; then
+		message=`ssh $4 "/usr/lib/nagios/plugins/check_file_age -w $warn -c $crit -f ${file}"`
+	else
+		message=`/usr/lib/nagios/plugins/check_file_age -w $warn -c $crit -f "$file"`
+	fi
 
-rm -f ${STATUS_TXT}
+
+	if [ "$?" != "0" ] ; then
+		echo "$message"
+		exit $?
+	fi
+}
+
+STATE_OK=0
+STATE_WARNING=1
+STATE_CRITICAL=2
+STATE_UNKNOWN=3
+
+exitCode=$STATE_UNKNOWN
+MESSAGE=""
+
+if [ -n "$1" ] ; then
+	SERVER=$1
+
+	BACKUP_TXT="/tmp/$SERVER-backups.txt"
+	STATUS_TXT="/tmp/$SERVER-status.txt"
+	MAILBOXES_TXT="/tmp/originalmailboxes.txt"
+
+	rm -f ${STATUS_TXT}
 
 
-if [ "$SERVER" == "localhost" ] ; then
-	su zimbra -l -c "zmbackupquery -v $SERVER |grep @ | cut -d ' ' -f 3- | cut -d ':' -f 1| sort |uniq > ${BACKUP_TXT}"
-	checkexitstatus
-	su zimbra -l -c "comm -23 <(sort ${MAILBOXES_TXT}) <(sort ${BACKUP_TXT})" > ${STATUS_TXT}
-	checkexitstatus
+	if [ "$SERVER" == "localhost" ] ; then
+		su zimbra -l -c "zmbackupquery -v $SERVER |grep @ | cut -d ' ' -f 3- | cut -d ':' -f 1| sort |uniq > ${BACKUP_TXT}"
+		checkexitstatus
+		checkfileage ${BACKUP_TXT} 60 60
+		checkfileage ${MAILBOXES_TXT} $((25 * 60 * 60)) $((32 * 60 * 60))
+
+		su zimbra -l -c "comm -23 <(sort ${MAILBOXES_TXT}) <(sort ${BACKUP_TXT})" > ${STATUS_TXT}
+		checkexitstatus
+	else
+		ssh zimbra@$SERVER "zmbackupquery -v $SERVER |grep @ | cut -d ' ' -f 3- | cut -d ':' -f 1| sort |uniq > ${BACKUP_TXT}"
+		checkexitstatus
+		checkfileage ${BACKUP_TXT} 60 60 zimbra@$SERVER
+		checkfileage ${MAILBOXES_TXT} $((25 * 60 * 60)) $((32 * 60 * 60)) zimbra@$SERVER
+
+		ssh zimbra@$SERVER "comm -23 <(sort ${MAILBOXES_TXT}) <(sort ${BACKUP_TXT})" > ${STATUS_TXT}
+		checkexitstatus
+
+	fi
+	checkfileage ${STATUS_TXT} 60 60
+
+
+	if [ $(wc -l ${STATUS_TXT} |cut -f 1 -d " ") -gt 0 ] ; then
+		MESSAGE="$SERVER: $(wc -l ${STATUS_TXT} |cut -f 1 -d " ") accounts do not have a backup: `cat ${STATUS_TXT} | sed ':a;N;$!ba;s/\n/ /g'`"
+		exitCode=$STATE_CRITICAL
+	else
+		MESSAGE="$SERVER: all accounts have a backup"
+		exitCode=$STATE_OK
+	fi
+
 else
-	ssh zimbra@$SERVER "zmbackupquery -v $SERVER |grep @ | cut -d ' ' -f 3- | cut -d ':' -f 1| sort |uniq > ${BACKUP_TXT}"
-	checkexitstatus
-	ssh zimbra@$SERVER "comm -23 <(sort ${MAILBOXES_TXT}) <(sort ${BACKUP_TXT})" > ${STATUS_TXT}
-	checkexitstatus
+    exitCode=$STATE_UNKNOWN
 
+    checkParam "server" "$1"
+ 
+    trimMessage
+    MESSAGE="Missing paramaters ${MESSAGE} ($@)"
 fi
 
-if [ $(wc -l ${STATUS_TXT} |cut -f 1 -d " ") -gt 0 ]
-        then
-                echo "$SERVER: $(wc -l ${STATUS_TXT} |cut -f 1 -d " ") accounts do not have a backup: `cat ${STATUS_TXT} | sed ':a;N;$!ba;s/\n/ /g'`"
-                CHECK=Failed
-        else
-                echo "$SERVER: all accounts have a backup"
-                CHECK=OK
+trimMessage
+
+if [ $exitCode -eq $STATE_UNKNOWN ]; then
+    echo "UNKNOWN - $MESSAGE"
+elif [ $exitCode -eq $STATE_CRITICAL ]; then
+    echo "CRITICAL - $MESSAGE"
+elif [ $exitCode -eq $STATE_WARNING ]; then
+    echo "WARNING - $MESSAGE"
+elif [ $exitCode -eq $STATE_OK ]; then
+    echo "OK - $MESSAGE"
 fi
 
-if [[ "$CHECK" == "OK" ]]; then
-   exit 0
-elif [[ "$CHECK" == "Failed" ]]; then
-   exit 2
-else
-   exit 255
-fi
+exit $exitCode
