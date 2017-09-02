1b91221c4204920beeec00c73b2f132525fcf5f1
Oliver Lowe <oliver.lowe@sol1.com.au>
2017-09-02 16:23:14 +1000

    Add check remote port option
    So now we can check both the remote address AND the remote port

--- check_winsock/check_winsock.go
+++ check_winsock/check_winsock.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
@@ -9,16 +10,20 @@ import (
 	"mig.ninja/mig/modules/netstat"
 )
 
+const UNKNOWN  int = 127
+const CRITICAL int = 2
+const WARNING  int = 1
+const OK       int = 0
 
 func usage() {
-	fmt.Printf("usage: check_winsock address\n")
+	fmt.Printf("usage: check_winsock [-p port] address\n")
 }
 
 /* 
- * procstat returns true if the process proc is currently running, or false
- * otherwise. Error is returned on unsuccessful querying of running processes.
+ * ProcStat returns true if the process proc is currently running, or false
+ * otherwise. Error is returned on unsuccessful listing of running processes.
  */
-func procstat(proc string) (running bool, err error) {
+func ProcStat(proc string) (running bool, err error) {
 	cmdstring := fmt.Sprintf("tasklist /fi \"imagename eq %s\"", proc)
 	cmd := exec.Command(cmdstring)
 	o, err := cmd.Output()
@@ -34,20 +39,50 @@ func main() {
 		usage()
 		os.Exit(1)
 	}
-	/* Check if process is running */
+	/*
+	 * TODO(olly): Check if process is running using ProcStat(). If not,
+	 * then don't even worry about checking sockets.
+	 */
 
+	expectedport := flag.Int("p", 0, "port number")
+	process := flag.String("n", "", "process name")
+	flag.Parse()
 	addr := os.Args[1]
-	isconnected, elements, err := netstat.HasIPConnected(addr)
+
+	if *process != "" {
+		fmt.Printf("warning: the -n option is not yet implemented\n")
+	}
+
+	isconnected, sockets, err := netstat.HasIPConnected(addr)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error on HasIPConnected(): %v\n", err)
 	}
 
+	status := UNKNOWN
 	if isconnected {
-		for i := 0; i < len(elements); i++ {
-			port := elements[i].RemotePort
-			fmt.Printf("Connected to %s on %d\n", addr, int(port))
+		fmt.Printf("established socket with %s\n", addr)
+		/* We don't care about remote ports if the option isn't set. */
+		if expectedport == 0 {
+			status = OK
+			break
 		}
-	} else {
+		/* Check that the remote port of each socket is as expected. */
+		for i := 0; i <= len(sockets); i++ {
+			remoteport := sockets[i].RemotePort
+			if remoteport == expectedport {
+				status = OK
+				fmt.Printf("Socket uses expected port %d\n",
+				    *expectedport)
+			} else if remoteport != expectedport {
+				status = CRITICAL
+				fmt.Printf("Socket established with %s, ",
+				    "but using port %d (not %d)",
+				    addr, remoteport, *expectedport)
+			}
+		}
+	} else if isconnected == false {
+		status = WARNING
 		fmt.Printf("No connection to %s\n", addr)
 	}
+	os.Exit(status)
 }
