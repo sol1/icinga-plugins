d553004209e6bc26d5b7af83cb42709cb8da885c
Oliver Lowe <oliver.lowe@sol1.com.au>
2017-09-06 19:14:01 +1000

    Major rewrite with help from matthew.streatfield@

--- check_winsock/check_winsock.go
+++ check_winsock/check_winsock.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
 	"os"
-	"os/exec"
+	"fmt"
+	"bufio"
 	"strings"
-
-	"mig.ninja/mig/modules/netstat"
 )
 
 const UNKNOWN  int = 127
@@ -20,31 +19,102 @@ func usage() {
 }
 
 /* 
- * ProcStat returns true if the process proc is currently running, or false
- * otherwise. Error is returned on unsuccessful listing of running processes.
+ * CheckConnected returns true if the host is currently connected to the address
+ * and port, or false otherwise. Error is returned on unsuccessful listing of
+ * network info.
  */
-func ProcStat(proc string) (running bool, err error) {
-	filter := fmt.Sprintf("imagename eq %s", proc)
-	cmd := exec.Command("tasklist", "-fi", filter)
-	o, err := cmd.CombinedOutput()
+func CheckConnectedPort(addr, port string)(connected bool, err error){
+	file, err := os.Open("output.txt");
 	if err != nil {
 		return false, err
 	}
-	output := string(o)
-	return strings.Contains(output, proc), nil
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		cols := strings.Fields(scanner.Text())
+		// ignore UDP and empty lines.
+		if len(cols) > 0 && cols[0] == "TCP" {
+			longaddr := strings.Split(cols[2], ":")
+			if longaddr[0] == addr && longaddr[1] == port {
+				return true, nil
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return false, err
+}
+
+/* 
+ * CheckConnectedProc returns true if the process is currently connected to the address
+ * and port, or false otherwise. Error is returned on unsuccessful listing of
+ * network info.
+ */
+func CheckConnectedProc(proc, addr, port string)(connected bool, err error){
+	file, err := os.Open("outputb.txt");
+	if err != nil {
+		return false, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var matchedaddr bool = false
+	var curproc string = ""
+	for scanner.Scan() {
+		cols := strings.Fields(scanner.Text())
+		if len(cols) > 0 && cols[0] == "TCP" {
+			// proto = cols[0]
+			// localconn = cols[1]
+			remotehost := cols[2]
+			longaddr := strings.Split(remotehost, ":")
+			/*
+			 * Check entry contains the address we're interested in.
+			 * Otherwise, move on to next line.
+			 */
+			if longaddr[0] == addr && longaddr[1] == port {
+				matchedaddr = true
+				continue
+			}
+			continue
+		}
+
+		if len(cols) > 0 && strings.HasPrefix(cols[0], "[") {
+			curproc = cols[0]
+		}
+
+		/*
+		 * Check the socket parent process is as requested. We
+		 * compare strings in the same format as returned by
+		 * netstat with the 'b' option on windows:
+		 * "[processname.exe]"
+		 */
+		procf := fmt.Sprintf("[%s]", proc)
+		if curproc != procf {
+			matchedaddr = false
+		} else if matchedaddr && curproc == procf {
+			return true, nil
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+	    log.Fatal(err)
+	}
+	return false, err
 }
 
 func main() {
 	var status int
 	var addr string
 
-
 	if len(os.Args) < 2 {
 		usage()
 		os.Exit(1)
 	}
 
-	expectedport := flag.Int("p", 0, "port number")
+	expectedport := flag.String("p", "0", "port number")
 	process := flag.String("n", "", "process name")
 	flag.Parse()
 	addr = flag.Arg(0)
@@ -54,63 +124,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	/*
-	 * TODO(olly): Check if process is running using ProcStat(). If not,
-	 * then don't even worry about checking sockets.
-	 */
 	status = UNKNOWN
-	if *process != "" {
-		fmt.Printf("warning: the -n option is not implemented yet\n")
-		isrunning, err := ProcStat(*process)
-		if err != nil {
-			fmt.Fprintf(os.Stderr,
-			    "warning: Error querying process list: %v\n", err)
-		}
-		if isrunning {
-			fmt.Fprintf(os.Stderr, "%s is running\n", *process)
-		} else if isrunning == false {
-			fmt.Fprintf(os.Stderr,
-			    "warning: %s not running\n", *process)
-		}
-	}
 
-	isconnected, sockets, err := netstat.HasIPConnected(addr)
+	var connected bool
+	var err error
+	connected, err = CheckConnectedPort(addr, *expectedport)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error on HasIPConnected(): %v\n", err)
+		log.Fatal(err)
 	}
 
-	if isconnected {
-		fmt.Printf("active connection with %s\n", addr)
-		/* Check that the remote port is as expected. */
-		for i := 0; i < len(sockets); i++ {
-			/*
-			 * We don't care about remote ports if the option isn't
-			 * set, so just return OK.
-			 */
-			if *expectedport == 0 {
-				status = OK
-				break
-			}
-			/* cast to int. Unknown why pkg netstat returns float64. */
-			remoteport := int(sockets[i].RemotePort)
-			if remoteport == *expectedport {
-				status = OK
-				fmt.Printf("socket uses expected port: %d\n",
-				    remoteport)
-				break
-			} else if remoteport != *expectedport {
-				status = CRITICAL
-				fmt.Printf("socket uses unexpected port: %d\n",
-				    remoteport)
-			} else {
-				status = UNKNOWN
-				fmt.Printf("Cannot compare remote port with",
-				    "expected port\n")
-			}
+	if *process != "" {
+		connected, err = CheckConnectedProc(*process, addr, *expectedport)
+		if err != nil {
+			log.Fatal(err)
 		}
-	} else if isconnected == false {
-		status = WARNING
-		fmt.Printf("No connection to %s\n", addr)
 	}
+
+	if connected {
+		status = OK
+	} else {
+		status = CRITICAL
+	}
+
 	os.Exit(status)
 }
