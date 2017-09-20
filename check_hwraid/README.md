3629d5e3dfa120e49af402935571c66738c4665a
Oliver Lowe <oliver.lowe@sol1.com.au>
2017-09-20 11:18:14 +1000

    Import check_hwraid for windows. Documentation coming!!

---
+++ check_hwraid/README.md
@@ -0,0 +1,39 @@
+RAIDCAT(1) - General Commands Manual
+
+# NAME
+
+**raidcat** - does stuff
+
+# SYNOPSIS
+
+**raidcat**
+\[**-t**&nbsp;**-devtype**]
+*path*
+
+# DESCRIPTION
+
+**raidcat**
+Parses raidcat data...?
+
+The options are as follows:
+
+**-t**
+
+> Set the device type. The default is...
+
+# EXIT STATUS
+
+**raidcat**
+exits with good exit codes
+
+# EXAMPLES
+
+Check an intel raid device...
+
+	$ raidcat -t intel
+
+# SEE ALSO
+
+icinga2(1)
+
+OpenBSD 6.1 - September 19, 2017
---
+++ check_hwraid/raidcat.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+)
+
+type Controller struct {
+	Family string
+	State	string
+}
+
+const OK string = "OK"
+const DEGRADED string = "DEGRADED"
+const REBUILDING string = "REBUILDING"
+
+const K    int = 0
+const WARN int = 1
+const CRIT int = 2
+const UNKNOWN int = 3
+
+func ControllerStat(helper string) (cmsg []byte, err error) {
+	// TODO(olly): ensure full path to the helper script is passed to
+	// exec.Command(), not just name of script.
+	output, err := exec.Command(helper).Output()
+	if err != nil {
+		log.Printf("ControllerStat(): Error running %s: %v", helper, err)
+		return nil, err
+	}
+	return output, nil
+}
+
+func main() {
+	var ctype = flag.String("t", "", "family type of RAID controller")
+	flag.Parse()
+	var cmesg []byte
+
+	// Read incoming message from controller from either standard input or
+	// from the standard output of a specified helper program.
+	if *ctype == "" {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			cmesg = scanner.Bytes()
+		}
+	} else {
+		var err error
+		cmesg, err = ControllerStat(*ctype)
+		if err != nil {
+			fmt.Printf("Could not run helper for %s\n", *ctype)
+			os.Exit(UNKNOWN)
+		}
+	}
+
+	var c Controller
+	if err := json.Unmarshal(cmesg, &c); err != nil {
+		fmt.Printf("Error unmarshalling json: %v\n", err)
+		os.Exit(UNKNOWN)
+	}
+
+	if c.State == OK {
+		fmt.Printf("%s reports state is %s\n", c.Family, c.State)
+		os.Exit(K)
+	}
+	if c.State != OK {
+		fmt.Printf("controller %s state is %s\n", c.Family, c.State)
+		os.Exit(CRIT)
+	}
+}
