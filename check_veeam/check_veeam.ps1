32ff37d5273b890e66f870fc77c1ba595e10e417
Oliver Lowe <oliver.lowe@sol1.com.au>
2017-10-05 10:57:12 +1100

    Get last result of every veeam job.
    Instead of passing in the job name as an argument at runtime, just query the
    veeam database and get every job's last result status.

--- check_veeam/check_veeam.ps1
+++ check_veeam/check_veeam.ps1
@@ -1,21 +1,14 @@
 asnp VeeamPSSnapin
 
-$period = 1
-
-$jobs = Get-VBRJob | Select name
-For ($i = 0; $i -lt $jobs.length; $i++ {
-	$job = Get-VBRJob -Name $jobs[$i]
+# Get an array of veeam jobs called 'jobs', then loop through each job querying
+# its last result.
+$jobs = Get-VBRJob
+For ($i = 0; $i -lt $jobs.length; $i++) {
+	$j = $jobs[$i]
 	$status = $job.GetLastResult()
 	switch ($status) {
-	"Failure" { Write-Host "Last run of job $name failed"; exit 2 }
-	"Success" { Write-Host "Last run of job $job succeeded"; exit 0 }
-	default   { Write-Host "Unknown last result of job $job"; exit 3 }
+	"Failure" { Write-Host "Last run of job" $j.Name "failed" }
+	"Success" { Write-Host "Last run of job" $j.Name "succeeded" }
+	default   { Write-Host "Cannot determine state of last run of" $j.Name }
 	}
 }
-
-$now = (Get-Date).AddDays(-$period)
-$now = $now.ToString("yyyy-MM-dd")
-$last = $job.GetScheduleOptions()
-$last = $last -replace '.*Latest run time: \[', ''
-$last = $last -replace '\], Next run time: .*', ''
-$last = $last.split(' ')[0]
