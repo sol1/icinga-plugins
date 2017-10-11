aa94c0a4428cdee6f3f6f2a12e9c62fda55ff956
Oliver Lowe <oliver.lowe@sol1.com.au>
2017-10-11 11:38:32 +1100

    More verbosity on all statuses

--- check_veeam/check_veeam.ps1
+++ check_veeam/check_veeam.ps1
@@ -4,6 +4,8 @@ param (
 
 asnp VeeamPSSnapin
 
+# Get-JobResult takes a Veeam backup job as an arugment, and returns the result
+# of the most recent run.
 function Get-JobResult {
 	$job = $args[0]
 	$session = Get-VBRBackupSession | where {$_.jobID -eq $job.Id.Guid} | Sort EndTimeUTC -Descending | Select -First 1
@@ -18,24 +20,27 @@ if ($name) {
 	$jobs = Get-VBRJob
 }
 
-$numfail, $numwarn = 0
+$numfail = 0
+$numwarn = 0
 for ($index = 0; $index -lt $jobs.length; $index++) {
 	$currentjob = $jobs[$index]
 	$lastresult = Get-JobResult $currentjob
 	switch ($lastresult) {
-	"Success" { continue }
-	"Warning" { Write-Host "job" $currentjob.Name "finished with warning" }
+	"Success" { Write-Host "job" $currentjob.Name "succeeded"; continue }
+	"Warning" { Write-Host "job" $currentjob.Name "finished with warning"; $numwarn++ }
 	"Failed"  { Write-Host "job" $currentjob.Name "failed"; $numfail++ }
 	default   { Write-Host "Cannot determine status of job" $currentjob.Name }
 	}
 }
 
 if ($numfail -gt 0) {
+	echo "CRITICAL: Backup jobs failed"
 	exit 2
 } elseif ($numwarn -gt 0) {
+	echo "WARNING: Some backups completed with warnings"
 	exit 1
 } elseif (($numfail -eq 0) -and ($numwarn -eq 0)) {
-	echo "veeam backups are ok :)"
+	echo "OK: Veeam backups succeeded"
 	exit 0
 }
 exit 3
